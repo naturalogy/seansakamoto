2116cdb32933c5eef46bceeb6eaa1230a9a6a946
Sean 唱 Sakamoto <naturalogy@users.noreply.github.com>
2026-04-09 13:37:23 +0900

    Configure basePath and assetPrefix for production
    Add repository name and configure basePath and assetPrefix for production.

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,19 @@
 /** @type {import('next').NextConfig} */
 
 const isProd = process.env.NODE_ENV === 'production'
+const repositoryName = 'seansakamoto'
 const nextConfig = {
   reactStrictMode: isProd,
   output: 'export',
   images: {
     unoptimized: true,
   },
+  ...(isProd
+    ? {
+        basePath: `/${repositoryName}`,
+        assetPrefix: `/${repositoryName}/`,
+      }
+    : {}),
   poweredByHeader: false,
   turbopack: {},
   webpack: (webpackConfig) => {
